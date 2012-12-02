e96f959153eeb1627223971f2437bd9bba9d70d3
Roberto Acevedo <rxacevedo@gmail.com>
2012-12-02 18:13:57 +0000

    No more old client

--- client.c
+++ client.c
@@ -1,74 +1,75 @@
+/*
+ * Roberto Acevedo
+ * Multi-threaded socket server project
+ * COP4610
+ * client.c
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <pthread.h>
+#include <unistd.h>
 
-void error(const char *msg)
-{
-  perror(msg);
-  exit(0);
-}
+#define BUFFER_SIZE 256 
 
 int main(int argc, char *argv[])
 {
+  int sockfd, rw;
+  struct addrinfo flags;
+  struct addrinfo *server_info;
 
-  int sockfd, portno, n;
-  struct sockaddr_in serv_addr;
-  struct hostent *server;
+  char *message = malloc(BUFFER_SIZE); 
 
-  char buffer[256];
+  if (argc < 3) perror("Usage: ./server <hostname/address> <port/service type>");
+  
+  memset(&flags, 0, sizeof(flags)); // Clear to we're not working with garbage
+  flags.ai_family = AF_UNSPEC; // IPv4 or IPv6 doesn't matter
+  flags.ai_socktype = SOCK_STREAM; // TCP
+  flags.ai_flags = AI_PASSIVE; // get the IP for me
 
-  if (argc < 3) {
-    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+  if (getaddrinfo(argv[1], argv[2], &flags, &server_info) < 0) { // Resolve host based on CMD args
+    perror("Couldn't find host");
+    exit(-1);
+  }
+
+  sockfd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol); // Initialize socket
+
+  if (connect(sockfd, server_info->ai_addr, server_info->ai_addrlen) < 0)
+  {
+    perror("Couldn't conenct...");
+    exit(-1);
+  } 
+
+  printf("Connection established, please enter a message:\n");
+  bzero(message, BUFFER_SIZE);
+  fgets(message, BUFFER_SIZE-1, stdin);
+
+  rw = write(sockfd, message, strlen(message)); // Sending the contents of the buffer - writes using socket file descriptor
+  if (rw < 0) 
+  {
+    perror("Failed to send message.");
+    exit(-1);
+  }
+
+  bzero(message, BUFFER_SIZE);
+  rw = read(sockfd, message, BUFFER_SIZE); // Reading FROM socket file descriptor
+                                           // into the message buffer FOR the size 
+                                           // of message buffer MINUS ONE, otherwise
+                                           // the null terminator at the end will
+                                           // blank out the message
+
+  if (rw < 0)
+  {
+    perror("ERROR: Can't read from socket");
     exit(0);
   }
 
-  portno = atoi(argv[2]);
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  printf("The message is: %s\n", message);
 
-  if (sockfd < 0) 
-    error("ERROR opening socket");
-  server = gethostbyname(argv[1]);
-
-  if (server == NULL) {
-    fprintf(stderr,"ERROR, no such host\n");
-    exit(0);
-  }
-
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-
-  bcopy((char *)server->h_addr, 
-      (char *)&serv_addr.sin_addr.s_addr,
-      server->h_length);
-
-  serv_addr.sin_port = htons(portno);
-
-  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-    error("ERROR connecting");
-
-  printf("Please enter the message: ");
-
-  bzero(buffer,256);
-  fgets(buffer,255,stdin);
-
-  n = write(sockfd,buffer,strlen(buffer));
-
-  if (n < 0) 
-    error("ERROR writing to socket");
-
-  bzero(buffer,256);
-  n = read(sockfd,buffer,255);
-
-  if (n < 0) 
-    error("ERROR reading from socket");
-
-  printf("%s\n",buffer);
   close(sockfd);
-
   return 0;
+
 }
