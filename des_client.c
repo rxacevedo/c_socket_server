f6d4866d5ff31ed13c614f714144ebf23ef600f9
Roberto Acevedo <rxacevedo@gmail.com>
2012-12-01 21:25:53 -0500

    Client updaet: no longer using sockaddr_in at all, WOOT

--- des_client.c
+++ des_client.c
@@ -14,56 +14,44 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#define BUFFER_SIZE 256
-
-void error(const char *msg)
-{
-  perror(msg);
-  exit(0);
-}
+#define BUFFER_SIZE sizeof(char)*140
 
 int main(int argc, char *argv[])
 {
-  int sockfd, portno, rw;
-  struct sockaddr_in *destination;
+  int sockfd, rw;
+  // struct sockaddr_in *destination;
   struct addrinfo hints;
   struct addrinfo *server_info;
 
   char* message = malloc(sizeof(char)*140); // Twitter character limit 
-  // char dest_address[256] = "10.0.1.20";
-  // server = inet_addr("10.0.1.20");
+
+  if (argc < 2) perror("Usage: ./server <hostname/address> <port/service type>");
   
   memset(&hints, 0, sizeof(hints)); // Clear to we're not working with garbage
   hints.ai_family = AF_UNSPEC; // IPv4 or IPv6 doesn't matter
   hints.ai_socktype = SOCK_STREAM; // TCP
   hints.ai_flags = AI_PASSIVE; // get the IP for me
-  portno = 8080;
 
-  // Initialize socket and check it
-  // if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-  if (getaddrinfo(argv[1], "8080", &hints, &server_info) < 0) {
-    error("Couldn't find host.");
+  // Try and resolve host from argv[1]
+
+  if (getaddrinfo(argv[1], argv[2], &hints, &server_info) < 0) {
+    perror("Couldn't find host");
     exit(0);
   }
 
+  // Initialize socket and check it
   sockfd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
-  // connect to server
-  bzero(&destination, sizeof(&destination)); // Zero server address to prevent 
-                                                     // it initializing to crap (for the
-                                                     // size of the struct)
+  // Connect to server
+  // bzero(&destination, sizeof(&destination)); // Zero server address to prevent 
+                                             // it initializing to crap (for the
+                                             // size of the struct)
 
-  destination = (struct sockaddr_in *) server_info->ai_addr;
-
-  // destination.sin_family = AF_INET; // Making Internets
-  // destination.sin_addr.s_addr = inet_addr("10.0.1.30");
-  // destination.sin_addr.s_addr = inet_addr(server_info->ai_addr);
-  // destination.sin_port = htons(portno); // Converting portno to network byte order
-  // connect(sockfd, (struct sockaddr *) &destination, sizeof(destination));
+  // destination = (struct sockaddr_in *) server_info->ai_addr;
 
   if (connect(sockfd, server_info->ai_addr, server_info->ai_addrlen) < 0)
   {
-    error("Couldn't conenct...");
+    perror("Couldn't conenct...");
     exit(0);
   } 
 
@@ -74,7 +62,7 @@ int main(int argc, char *argv[])
   rw = write(sockfd, message, strlen(message)); // Sending the contents of the buffer - writes using socket file descriptor
   if (rw < 0) // Is this the length of the data sent out, or an stderr?
   {
-    error("Failed to send message.");
+    perror("Failed to send message.");
   }
 
   // get & process response
@@ -87,7 +75,7 @@ int main(int argc, char *argv[])
 
   if (rw < 0)
   {
-    error("ERROR: Can't read from socket");
+    perror("ERROR: Can't read from socket");
     exit(0);
   }
 
