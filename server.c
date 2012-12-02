b3a7e0cbac634bee7148678668cc14b95d7d2507
Roberto Acevedo <rxacevedo@gmail.com>
2012-12-02 00:40:29 -0500

    Moving to newer, IPv6 complient code

--- server.c
+++ server.c
@@ -12,48 +12,50 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <netdb.h>
 
 /* Preprocessor Directives */
 
 #define NTHREADS 100
 #define QUEUE_SIZE 5
+#define BUFFER_SIZE 256
 #define TRUE 1
 
 /* Globals */
 
 // sem_t thread_sem[NTHREADS];
-// pthread_t chkthread;
 pthread_mutex_t lock;
-int serviced_count = 0;
+int counter = 0;
 
 void *threadalizer(void *arg)
 {
-  int fd, rw; // File descriptor and 'read/write' to socket indicator
-  char buffer[256];
-  fd = (int) arg;
+  int sockfd, rw; // File descriptor and 'read/write' to socket indicator
+  char *buffer;
+  sockfd = (int) arg;
 
-  bzero(buffer,256);
-  rw = read(fd, buffer, sizeof(buffer)-1); // Blocks until there is something to be read in the socket
+  buffer = malloc(BUFFER_SIZE);
+  bzero(buffer, BUFFER_SIZE);
+  rw = read(sockfd, buffer, sizeof(buffer)-1); // Blocks until there is something to be read in the socket
 
   if (rw < 0) perror("ERROR reading from socket");
 
-  printf("Here is the message: %s\n", &buffer);
+  printf("Here is the message: %s\n", buffer);
   bzero(buffer, sizeof(buffer)-1);
 
-  sprintf(buffer, "Acknowledgement from thread 0x%x", pthread_self()); // Thread IDs aren't really castable
-                                                                       // since they are opaque objects, 
-                                                                       // but this at least proves some way
-                                                                       // to identify threads
+  sprintf(buffer, "Acknowledgement from thread 0x%x", pthread_self()); // Thread IDs aren't meaningfully 
+                                                                       // castable since they are opaque 
+                                                                       // objects, but this at least provides
+                                                                       // some way to identify threads
 
-  rw = write(fd, buffer, sizeof(buffer)-1); // Minus one so as to not read null terminator
+  rw = write(sockfd, buffer, sizeof(buffer)-1); // Minus one so as to not read null terminator
 
   if (rw < 0) perror("ERROR writing to socket");
 
   pthread_mutex_lock (&lock); // Critical area
-  serviced_count++;
+  counter++;
   pthread_mutex_unlock (&lock);
 
-  close(fd);
+  close(sockfd);
   printf("Request for thread 0x%x served.\n", pthread_self());
   pthread_exit(0);
 }
@@ -61,46 +63,62 @@ void *threadalizer(void *arg)
 
 int main(int argc, char *argv[])
 {
-  int serv_sockfd, new_sockfd, portno;
-  socklen_t clilen;
-  struct sockaddr_in serv_addr, cli_addr;
+
+  int serv_sockfd, new_sockfd;
+  //  , portno;
+  struct addrinfo flags;
+  struct addrinfo *host_info;
+  socklen_t addr_size; // TODO: Don't need this, use addrinfo->ai_addrlen instead!
+  // struct sockaddr_in serv_addr, cli_addr; // Don't need these either
+  struct sockaddr_storage client;
   pthread_attr_t attr;
   pthread_t threadid[NTHREADS];
 
   if (argc < 2) {
     fprintf(stderr,"ERROR, no port provided\n");
-    exit(1);
+    exit(-1);
   }
 
-  serv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  // HOST INFO HERE
+  if (getaddrinfo(NULL, argv[1], &flags, &host_info) != 0)
+  {
+    perror("Couldn't read host info for socket starti");
+    exit(-1);
+  }
+
+  serv_sockfd = socket(host_info->ai_family, host_info->ai_socktype, host_info->ai_protocol);
 
   if (serv_sockfd < 0) perror("ERROR opening socket");
 
-  bzero((char *) &serv_addr, sizeof(serv_addr)); // Initializing serving address to 0 using 
-                                                 // bzero(), pass in pointer to buffer
-                                                 // and size of buffer
+  // bzero((char *) &serv_addr, sizeof(serv_addr)); // Initializing serving address to 0 using 
+                                                    // bzero(), pass in pointer to buffer
+                                                    // and size of buffer
 
-  portno = atoi(argv[1]); // Getting the port number from argv (command line argument)
-  serv_addr.sin_family = AF_INET; // Setting values in serv_addr struct with lib supplied constants
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
+  // portno = atoi(argv[1]); // Getting the port number from argv (command line argument)
+  // serv_addr.sin_family = AF_INET; // Setting values in serv_addr struct with lib supplied constants
+  // serv_addr.sin_addr.s_addr = INADDR_ANY;
+  // serv_addr.sin_port = htons(portno);
 
-  if (bind(serv_sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) perror("ERROR on binding");
+  if (bind(serv_sockfd, host_info->ai_addr, host_info->ai_addrlen) < 0) 
+  {
+    perror("ERROR on binding");
+    exit(-1);
+  }
 
-  printf("Server started, listening for connections...");
+  // printf("Server started, listening for connections...");
 
-  while (TRUE) {
-
+  while (TRUE) 
+  {
     listen(serv_sockfd, QUEUE_SIZE); // Pass in socket file descriptor and the size of the backlog queue 
-                                // (how many pending connections can be in queue while another request
-                                // is handled)
+                                     // (how many pending connections can be in queue while another request
+                                     // is handled)
 
     pthread_attr_init(&attr); // Creating thread attributes
     pthread_attr_setschedpolicy(&attr, SCHED_FIFO); // I want FIFO - test
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
-    clilen = sizeof(cli_addr);
-    new_sockfd = accept(serv_sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    addr_size = sizeof(client);
+    new_sockfd = accept(serv_sockfd, (struct sockaddr *) &client, &addr_size);
 
     if (new_sockfd < 0) perror("ERROR on accept");
 
