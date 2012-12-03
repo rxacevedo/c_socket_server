4f30b8e9805cc7571c7896f7c789566bf3b70ae1
Roberto Acevedo <rxacevedo@gmail.com>
2012-12-02 21:45:24 -0500

    OK fine

--- stress.js
+++ stress.js
@@ -4,8 +4,11 @@ var open = 0;
 
 setInterval(function () {
   var socket = net.connect({
-   host: 'arm.robertoacevedo.net',
-   port: 8080
+   // Substitute IP/port of machine you run server on
+   // Note: EOENT means the FS is no longer allowing node to connect,
+   // re-run the script and the server will resume handling requests
+   host: 'robertoacevedo.net',
+   port: 9000 
   });
   socket.on('connect', function () {
     open++;
@@ -16,7 +19,7 @@ setInterval(function () {
     open--;
     showOpen();
   });
-}, 20);
+}, 5);
 
 var showOpen = function () {
   process.stdout.write('\u001B[2J\u001B[0;0f');
