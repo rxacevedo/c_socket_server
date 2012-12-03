6145dfe4a1c13ea9a5f6272e98aa8560886c8f8e
Roberto Acevedo <rxacevedo@gmail.com>
2012-12-02 21:23:10 -0500

    Old server file, David broke it :(

--- stress.js
+++ stress.js
@@ -1,44 +1,26 @@
-#!/usr/bin/node
-var net = require('net'),
-    os = require('os');
-
-var host = "localhost",
-    port = 8080;
-
-if (process.argv[1])
-  host = process.argv[1];
-
-if (process.argv[2])
-  port = parseInt(process.argv[2], 10);
+var net = require('net');
 
 var open = 0;
 
 setInterval(function () {
-
   var socket = net.connect({
-   host: host,
-   port: port
+   host: '10.0.1.20',
+   port: 8080
+   // host: 'robertoacevedo.net',
+   // port: 9000
   });
-
   socket.on('connect', function () {
     open++;
     showOpen();
-    setTimeout(function () {
-    socket.end('data\n');
-    }, 500);
+    socket.write('data\n');
   });
-
-  socket.on('data', function (d) {
-    console.log('Got data: ', d);
-  });
-
   socket.on('end', function () {
     open--;
     showOpen();
   });
-}, 200);
+}, 20);
 
 var showOpen = function () {
   process.stdout.write('\u001B[2J\u001B[0;0f');
-  console.log("Open connection count: ", open);
+  console.log("Open", open);
 };
